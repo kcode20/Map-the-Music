936c2e809303722f8733d4823398c328c14fc47a
Brooks <kh.brooks02@gmail.com>
2024-10-31 15:22:14 -0400

    fix: eslint standards across files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 import importPlugin from "eslint-plugin-import";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -14,9 +21,11 @@ export default [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
       "react/destructuring-assignment": "off",
       "react/jsx-props-no-spreading": "off",
       "react/react-in-jsx-scope": "off",
+      "import/no-cycle": "off",
       "import/order": [
         "error",
         {
@@ -31,6 +40,19 @@ export default [
         },
       ],
       "import/prefer-default-export": "off",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        typescript: {},
+      },
     },
   },
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+import { useRef, useEffect } from "react";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
